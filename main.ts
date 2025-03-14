85b92af0eee98f426f65a65c5ebb33ea6c985e20
alexulb1 <109351274+alexulb1@users.noreply.github.com>
2025-03-14 14:17:04 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    for (let x = 0; x <= 5; x++) {
-        for (let y = 0; y <= 4; y++) {
-            led.plot(y, x)
-            basic.pause(100)
-            led.unplot(y, x)
+    status = 1
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    status = 0
+})
+let status = 0
+status = 0
+basic.forever(function () {
+    if (status == 1) {
+        for (let x = 0; x <= 5; x++) {
+            for (let y = 0; y <= 4; y++) {
+                led.plot(y, x)
+                basic.pause(100)
+                led.unplot(y, x)
+            }
         }
     }
-})
-basic.forever(function () {
-	
 })
